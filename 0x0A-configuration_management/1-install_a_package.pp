aef02c1de92c002ba21537db55a480f3409aa286
carlanjoroge <carlanjoroge16@gmail.com>
2024-01-21 22:52:18 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# install a specific version of flask (2.1.0)
-package {'flask':
-ensure => '2.1.0',
-provider => 'pip3'
+# installation of  flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip'
 }
